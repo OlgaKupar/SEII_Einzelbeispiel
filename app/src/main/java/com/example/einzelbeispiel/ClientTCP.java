a1f59136f881ffeaa1b15561ceeaccf0bf9bf3ec
OlgaKupar <Oljalya.topolja@gmail.com>
2023-03-12 18:08:13 +0100

    Created a class ClientTCP.java to communicate the server via TCP

---
+++ app/src/main/java/com/example/einzelbeispiel/ClientTCP.java
@@ -0,0 +1,28 @@
+package com.example.einzelbeispiel;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ClientTCP {
+
+    private String getServerAnswer (String mnr) throws IOException{
+
+        String serverAnswer;
+
+        Socket clientSocket = new Socket("se2-isys.aau.at", 53212);
+
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        outToServer.writeBytes(mnr);
+        serverAnswer = inFromServer.readLine();
+
+        return serverAnswer;
+    }
+
+
+}
